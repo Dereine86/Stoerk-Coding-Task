7ed278d570e6751598edd039a98cb68f3146902e
jce2si <johannes.scherle@de.bosch.com>
2025-03-31 11:32:33 +0200

    Added Receiver.hpp, accidently removed it previously

---
+++ inc/Receiver.hpp
@@ -0,0 +1,13 @@
+#ifndef RECEIVER_HPP
+#define RECEIVER_HPP
+
+#include "cstdint"
+
+constexpr unsigned maxPacketSize = 123u;
+struct Packet
+{
+    unsigned char data[maxPacketSize]; // packet data
+    uint8_t size;                      // size of data
+};
+
+#endif // RECEIVER_HPP
